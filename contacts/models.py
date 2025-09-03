4021b349ecccbf1ab8a19e8cb11b121907dbdf87
Ariel Rojas <arielalejandrorojas@gmail.com>
2025-09-02 22:18:16 -0400

    first commit

---
+++ contacts/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Contact(models.Model):
+    avatar = models.ImageField(null=True, blank=True, upload_to='contact')
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=50)
+    birthdate = models.DateField(null=True, blank=True)
+    phone = models.CharField(max_length=12, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True) # Se crea fecha de forma automatica
+
+    def __str__(self) -> str:
+        return self.name
